c8848b38616d1af8fca7dc31a0ee8bb551f16750
nellisonprg147 <nellison@students.mchenry.edu>
2016-10-31 12:53:13 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+    $("p").click(function(){
+        $(this).toggleClass("highlight");
+    });
+    $("#smallLetter").click(function(){
+        $("#text").css("font-size", "1em");
+    });
+    $("#mediumLeter").click(function(){
+        $("#text").css("font-size", "1.5em");
+    });
+    $("#lagreLetter").click(function(){
+        $("#text").css("font-size", "2em");
+    });
+    $("#buttonPOne").click(function(){
+        $("#pOne").toggleClass("circle");
+    });
+    $("#buttonPTwo").click(function(){
+        $("#pTwo").toggleClass("circle");
+    });
+    $("#buttonPThree").click(function(){
+        $("#pThree").toggleClass("circle");
+    });
+    $("#buttonG").click(function(){
+        $(".header").css("background-color","pink");
+        $("h1").html("IT'S A GIRL! :)");
+    });
+    $("#boldText").click(function(){
+        $("p").toggleClass("bold");
+    });
+});
